337f61cacd6ddd54fc4ce0efa60d918d542ddc49
saeed <mobin.20004b@gmail.com>
2026-02-25 17:34:50 +0330

    Add PrismaPostgresAdapter and update Prisma client configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPostgresAdapter } from '@prisma/adapter-ppg';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+const adapter = new PrismaPostgresAdapter({
+  connectionString: process.env.PRISMA_DIRECT_TCP_URL!,
+});
+
+  return new PrismaClient({ adapter });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
